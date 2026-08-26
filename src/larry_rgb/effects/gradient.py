6f8cefed24c4adcab0232bb9591193fa590bb8af
Albert Hopkins <marduk@letterboxes.org>
2026-08-26 18:31:43 -0500

    effects: new effect: gradient
    This is a simple gradient effect. It does not animations.

---
+++ src/larry_rgb/effects/gradient.py
@@ -0,0 +1,58 @@
+"""Gradient Effect"""
+
+from functools import cached_property
+
+from larry.color import Color, ColorList
+from larry.plugins import apply_plugin_filter
+from openrgb.orgb import Device  # type: ignore
+from openrgb.utils import RGBColor  # type: ignore
+
+from larry_rgb import hardware as hw
+from larry_rgb.config import Config
+
+
+class Effect:
+    """Gradient Effect"""
+
+    def __init__(self) -> None:
+        self.config: Config
+
+    async def reset(self, colors: ColorList, config: Config) -> None:
+        """Reset the effect"""
+        self.config = config
+        dominant = Color.dominant(colors, 10)
+
+        for device in self.rgb.openrgb_client.ee_devices:
+            self.color_device(device, dominant)
+
+    def is_alive(self) -> bool:
+        """Return True if the effect is running"""
+        return True
+
+    async def run(self, colors: ColorList, config: Config) -> None:
+        """Nothing to do here"""
+
+    @cached_property
+    def rgb(self) -> hw.RGB:
+        """Returns the RGB instance.
+
+        A (cached) property so we only instantiate it once, lazily
+        """  # pylint: disable=duplicate-code
+        if not hasattr(self, "config"):
+            raise RuntimeError("Effect has not been (re)set")
+
+        address_and_port = self.config.address
+        address, _, port_str = address_and_port.partition(":")
+        port = int(port_str) if port_str else 6742
+
+        return hw.RGB(address=address, port=port)
+
+    def color_device(self, device: Device, colors: ColorList) -> None:
+        """Set the given device's color to the given color"""
+        for zone in device.zones:
+            led_count = len(zone.leds)
+            gradient = list(Color.gradient2(colors[:led_count], led_count))
+            gradient = apply_plugin_filter(gradient, self.config.config)
+
+            for led, color in zip(zone.leds, gradient):
+                led.set_color(RGBColor(color.red, color.green, color.blue))
---
+++ tests/test_effects_gradient.py
@@ -0,0 +1,79 @@
+# pylint: disable=missing-docstring,unused-argument
+from unittest import IsolatedAsyncioTestCase
+from unittest.mock import Mock, patch
+
+from openrgb.utils import RGBColor  # type: ignore
+
+from larry_rgb import hardware
+from larry_rgb.config import Config
+from larry_rgb.effects import gradient
+
+from .lib import BLUE, GREEN, IMAGE_COLORS, RED, make_config
+
+
+class GradientEffectTests(IsolatedAsyncioTestCase):
+    async def test_reset_sets_config(self) -> None:
+        effect = gradient.Effect()
+        config = Config(make_config())
+
+        with patch("larry_rgb.effects.gradient.hw.RGB", autospec=True):
+            await effect.reset(IMAGE_COLORS, config)
+
+        self.assertEqual(effect.config, config)
+
+    async def test_reset_colors_devices(self) -> None:
+        effect = gradient.Effect()
+        config = Config(make_config())
+
+        with patch("larry_rgb.effects.gradient.hw.RGB", autospec=True) as mock_rgb:
+            mock_rgb.return_value.openrgb_client.ee_devices = [
+                Mock(zones=[]) for _ in range(3)
+            ]
+            with patch.object(
+                effect, "color_device", wraps=effect.color_device
+            ) as color_device:
+                await effect.reset(IMAGE_COLORS, config)
+
+        self.assertEqual(color_device.call_count, 3)
+
+    def test_is_alive_always_true(self) -> None:
+        effect = gradient.Effect()
+
+        self.assertTrue(effect.is_alive())
+
+    async def test_run(self) -> None:
+        # Just test that it exists
+        effect = gradient.Effect()
+        config = Config(make_config())
+
+        await effect.run(IMAGE_COLORS, config)
+
+    async def test_rgb_property(self) -> None:
+        effect = gradient.Effect()
+        config = Config(make_config())
+
+        with patch("larry_rgb.effects.gradient.hw.RGB", autospec=True):
+            await effect.reset(IMAGE_COLORS, config)
+
+        rgb = effect.rgb
+
+        self.assertIsInstance(rgb, hardware.RGB)
+
+    async def test_color_device(self) -> None:
+        effect = gradient.Effect()
+        config = Config(make_config())
+        device = Mock(zones=[Mock(leds=[Mock() for _ in range(5)]) for _ in range(3)])
+
+        with patch("larry_rgb.effects.gradient.hw.RGB", autospec=True):
+            await effect.reset(IMAGE_COLORS, config)
+
+        effect.color_device(device, [RED, GREEN, BLUE])
+
+        expected = [(255, 0, 0), (170, 85, 0), (0, 255, 0), (0, 170, 85), (0, 0, 255)]
+        expected = expected * 3
+        i = 0
+        for zone in range(3):
+            for led in range(5):
+                color = RGBColor(*expected[i])
+                device.zones[zone].leds[led].set_color.assert_called_once_with(color)
+                i += 1
