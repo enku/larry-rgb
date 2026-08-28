f531904402e6db9dbc736be96cac1b79fb1bf208
Albert Hopkins <marduk@letterboxes.org>
2026-08-28 12:43:18 -0500

    effects: new Effect: solid
    This is like the "gradient" effect except only sets a single color which
    defaults to the dominant color of the given colors.

---
+++ src/larry_rgb/effects/solid.py
@@ -0,0 +1,73 @@
+"""Solid color Effect"""
+
+import asyncio
+import logging
+
+from larry.color import Color, ColorList
+from larry.plugins import apply_plugin_filter
+
+from larry_rgb import hardware as hw
+from larry_rgb.config import Config
+
+logger = logging.getLogger(__package__)
+
+
+class Effect:
+    """Solid color Effect"""
+
+    def __init__(self) -> None:
+        self._lock = asyncio.Lock()
+        self._running = False
+
+    def is_alive(self) -> bool:
+        """Return True if the Effect is running"""
+        return self._running
+
+    async def run(self, colors: ColorList, config: Config) -> None:
+        """Run the effect"""
+        logger.debug("Running solid Effect")
+
+        if self.is_alive():
+            logger.debug("Effect is already running")
+            return
+
+        async with self._lock:
+            self._running = True
+
+        await self.reset(colors, config)
+
+    async def reset(self, colors: ColorList, config: Config) -> None:
+        """Reset the effect"""
+        logger.debug("Resetting solid Effect")
+
+        effect_config = config.effect.config
+        logger.debug("config: %s", f"{effect_config=}")
+
+        if color_name := effect_config.get("color", "").strip():
+            color = Color(color_name)
+        else:
+            color = Color.dominant(colors, 1)[0]
+
+        [color] = apply_plugin_filter([color], config.config)
+        logger.debug("color: %s", color)
+        color_all_rgbs(color, get_rgb(config))
+
+    async def stop(self) -> None:
+        """Stop the Effect"""
+        async with self._lock:
+            self._running = False
+
+
+def color_all_rgbs(color: Color, rgb: hw.RGB) -> None:
+    """Set all rgbs to the given color"""
+    logger.debug("Setting all LEDs color to %s", color)
+    rgb.set_color(color)
+
+
+def get_rgb(config: Config) -> hw.RGB:
+    """Return an RGB instance given the config"""
+    address_and_port = config.address
+    address, _, port_str = address_and_port.partition(":")
+    port = int(port_str) if port_str else 6742
+
+    return hw.RGB(address=address, port=port)
---
+++ tests/test_effects_solid.py
@@ -0,0 +1,99 @@
+# pylint: disable=missing-docstring
+from unittest import IsolatedAsyncioTestCase
+from unittest.mock import Mock, patch
+
+from larry.color import Color
+from unittest_fixtures import Fixtures, given
+
+import larry_rgb
+from larry_rgb.config import Config
+from larry_rgb.effects import get_effect
+
+from . import lib
+
+IMAGE_COLORS = lib.IMAGE_COLORS
+DOMINANT_COLOR = Color("#9c7e35")
+make_config = lib.make_config
+
+
+@patch("larry_rgb.effects.solid.color_all_rgbs")
+@patch("larry_rgb.effects.solid.get_rgb")
+class SolidEffectTests(IsolatedAsyncioTestCase):
+    async def test_with_dominant(self, get_rgb: Mock, color_all_rgbs: Mock) -> None:
+        # given the config to run the solid effect
+        config = make_config(effect="solid")
+
+        # when the effect is run
+        task = await larry_rgb.plugin(IMAGE_COLORS, config)
+        await task
+
+        # it colors the leds the expected color
+        get_rgb.assert_called_with(Config(config))
+        rgb = get_rgb.return_value
+
+        color_all_rgbs.assert_called_once_with(DOMINANT_COLOR, rgb)
+
+    async def test_with_color_from_config(
+        self, get_rgb: Mock, color_all_rgbs: Mock
+    ) -> None:
+        # given the config to run the solid effect, with configured color
+        config = make_config(**{"effect": "solid", "effect.solid.color": "white"})
+
+        # when the effect is run
+        task = await larry_rgb.plugin([], config)
+        await task
+
+        # it colors the leds the expected color
+        get_rgb.assert_called_with(Config(config))
+        rgb = get_rgb.return_value
+
+        color_all_rgbs.assert_called_once_with(Color("white"), rgb)
+
+
+@given(lib.clear_cache)
+class EffectIsAliveTests(IsolatedAsyncioTestCase):
+    # pylint: disable=unused-argument
+    def test_before_running(self, *, fixtures: Fixtures) -> None:
+        effect = get_effect("solid")
+
+        self.assertEqual(effect.is_alive(), False)
+
+    async def test_when_run(self, *, fixtures: Fixtures) -> None:
+        effect = get_effect("solid")
+
+        with patch.object(effect, "reset"):
+            await effect.run([], Config(make_config()))
+
+        self.assertEqual(effect.is_alive(), True)
+
+    async def test_when_stopped(self, *, fixtures: Fixtures) -> None:
+        effect = get_effect("solid")
+
+        with patch.object(effect, "reset"):
+            await effect.run([], Config(make_config()))
+
+        await effect.stop()
+
+        self.assertEqual(effect.is_alive(), False)
+
+
+@given(lib.clear_cache)
+class EffectRunTests(IsolatedAsyncioTestCase):
+    # SolidEffectTests already tests most of this
+    # pylint: disable=unused-argument
+    async def test_when_running_does_not_reset(self, *, fixtures: Fixtures) -> None:
+        # given the solid effect
+        effect = get_effect("solid")
+
+        # given the config to run the solid effect
+        config = make_config(effect="solid")
+
+        with patch.object(effect, "reset") as reset:
+            # when the effect is run once
+            await effect.run([], Config(config))
+
+            # and then run a second time
+            await effect.run([], Config(config))
+
+        # then the effect isn't reset
+        self.assertEqual(reset.call_count, 1)
