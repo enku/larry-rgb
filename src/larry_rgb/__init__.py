62f886710fb27eb8b2aa9886c2ba97fbb0001a6d
Albert Hopkins <marduk@letterboxes.org>
2026-08-29 09:42:56 -0500

    plugin: add some logging

--- src/larry_rgb/__init__.py
+++ src/larry_rgb/__init__.py
@@ -3,20 +3,25 @@
 import asyncio
 from typing import Any
 
+from larry import LOGGER
 from larry.color import ColorList
 from larry.config import ConfigType
 
 from larry_rgb.config import Config
 from larry_rgb.effects import current, get_effect
 
+logger = LOGGER.getChild(__name__)
+
 
 async def plugin(colors: ColorList, larry_config: ConfigType) -> asyncio.Task[Any]:
     """RGB plugin handler"""
     config = Config(larry_config)
+    logger.debug("Getting %s Effect", config.effect)
     effect = get_effect(config.effect)
 
     if current_effect := current.get():
         if current_effect != effect:
+            logger.debug("Stopping %s Effect", current_effect)
             await current_effect.stop()
 
     current.set(effect)
