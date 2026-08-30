2c11120ae22dd2298d3cd3e42c7be094533f21c9
Albert Hopkins <marduk@letterboxes.org>
2026-08-30 15:52:22 -0500

    tests: add compliance test for all Effects

--- tests/test_effects.py
+++ tests/test_effects.py
@@ -1,12 +1,15 @@
 # pylint: disable=missing-docstring,unused-argument
-from unittest import TestCase
+from unittest import IsolatedAsyncioTestCase, TestCase
 from unittest.mock import patch
 
 from unittest_fixtures import Fixtures, given, where
 
+from larry_rgb.config import Config
 from larry_rgb.effects import get_effect, list_effects
 
-from .lib import clear_cache, effects_entry_points
+from .lib import IMAGE_COLORS, clear_cache
+from .lib import config as config_f
+from .lib import effects_entry_points, rgb_client
 
 
 @given(clear_cache)
@@ -41,3 +44,32 @@ class TestListEffects(TestCase):
         names = list_effects()
 
         self.assertEqual(names, ["foo", "bar", "baz", "random", "dummy", "candy"])
+
+
+@given(config_f, rgb_client)
+class AllEffectsTests(IsolatedAsyncioTestCase):
+    async def test(self, *, fixtures: Fixtures) -> None:
+        for effect_name in list_effects():
+            with self.subTest(effect=effect_name):
+                await self.assert_good_effect(effect_name, fixtures.config)
+
+    async def assert_good_effect(self, effect_name: str, config: Config) -> None:
+        get_effect.cache_clear()
+
+        # we should be able to instantiate with no args
+        effect = get_effect(effect_name)
+
+        # it should start out not running
+        self.assertEqual(effect.is_alive(), False)
+
+        # we should be able to start it with colors and config
+        await effect.start(IMAGE_COLORS, config)
+        self.assertEqual(effect.is_alive(), True)
+
+        # we should be able to reset it with colors and config
+        await effect.reset(IMAGE_COLORS, config)
+        self.assertEqual(effect.is_alive(), True)
+
+        # we should be able to kill it
+        await effect.stop()
+        self.assertEqual(effect.is_alive(), False)
